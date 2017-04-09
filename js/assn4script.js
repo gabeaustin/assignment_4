b95c96b3ab35440b40796efc17d2a0d38d2c221c
Gabriel Austin <gabriel.austin1@gmail.com>
2017-04-09 14:19:00 -0400

    add alert msg
    added alert msg

--- js/assn4script.js
+++ js/assn4script.js
@@ -26,9 +26,49 @@ function CreateCustomer()
 	objRequest.send(newcustomer);
 }
 
+// This function will list the customers in a table
+function ListCustomers() {
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://student.business.uab.edu/jsonwebservice/service1.svc/CreateCustomer";
+	
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var output = JSON.parse(xmlhttp.responseText);
+			GenerateOutput(output);
+		}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+	
+	function GenerateOutput(result) {
+		var display = "<table><tr><th>Customer ID</th><th>Customer Name</th><th>Customer City</th></tr>";
+		var count = 0;
+		var rowid = "oddrow";
+		var customername = "";
+		var customerid = "";
+		for(count = 0; count < result.GetAllCustomersResult.length; count ++) {
+			if (count % 2 == 0) {
+				rowid = "evenrow";
+			} else {
+				rowid = "oddrow";
+			}
+			customername = '<a id="clickable" onclick="Customers(' + "'" + result.GetAllCustomersResult[count].CustomerID + "'); return false;" + "" + ">";
+			customername += result.GetAllCustomersResult[count].CustomerID;
+			customername += '</a>';
+			
+			display += "<tr id=" + rowid + "><td>" + customerid + "</td><td>" + customername + "</td><td>" + result.GetAllCustomersResult[count].City + "</td></tr>";
+			display += "</table>";
+			document.getElementById("customerid").innerHTML = display;
+		}
+	}
+	
+} // ends list customer function
+
+
+
 function OperationResult(output) {
 	if (output.WasSuccessful == 1) {
-		document.getElementById("result").innerHTML = "The operation was successful!"
+		document.getElementById("result").innerHTML = "<alert>" + "The operation was successful!" + "</alert>";
 	} else {
 		document.getElementById("result").innerHTML = "The operation was not successful." + "<br>" + output.Exception;
 	}
